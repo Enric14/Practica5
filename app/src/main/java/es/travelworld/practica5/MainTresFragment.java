d5eea18d321db8ba10c1280b9078fb057fb60d14
enric <enricpb96@gmail.com>
2023-07-10 22:18:55 +0200

    Fourth commit

---
+++ app/src/main/java/es/travelworld/practica5/MainTresFragment.java
@@ -0,0 +1,31 @@
+package es.travelworld.practica5;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import es.travelworld.practica5.databinding.FragmentMainTresBinding;
+
+public class MainTresFragment extends Fragment {
+
+    private FragmentMainTresBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentMainTresBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
